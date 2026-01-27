ddcca41e595bd63b5ee73c67826b6a85fb1bec11
zhangyipeng <67528767+ZypIsGod@users.noreply.github.com>
2026-01-27 16:17:57 +0800

    first commit

---
+++ java/com/zyp/demo1/NettyServer.java
@@ -0,0 +1,31 @@
+package com.zyp.demo1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Date:2026/1/26
+ * @Author：zyp
+ * @Description:
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+
+                    }
+                });
+        serverBootstrap.bind(8000);
+    }
+}
---
+++ java/com/zyp/demo1/NettyServer01.java
@@ -0,0 +1,53 @@
+package com.zyp.demo1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+/**
+ * @Date:2026/1/26
+ * @Author：zyp
+ * @Description:
+ */
+public class NettyServer01 {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+
+                    }
+                });
+        bind(serverBootstrap,8080);
+    }
+
+    public static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("端口【" + port + "】绑定成功");
+                } else {
+                    System.out.println("端口【" + port + "】绑定失败");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+        serverBootstrap.handler(new ChannelInitializer<NioServerSocketChannel>() {
+            @Override
+            protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+                System.out.println("服务端启动中");
+            }
+        });
+    }
+}
