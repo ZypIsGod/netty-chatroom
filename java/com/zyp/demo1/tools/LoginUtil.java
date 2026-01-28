8801b23dec3eab05e2faa836c3cf9aa027389e86
zhangyipeng <67528767+ZypIsGod@users.noreply.github.com>
2026-01-28 18:02:56 +0800

    first commit

--- java/com/zyp/demo1/tools/LoginUtil.java
+++ java/com/zyp/demo1/tools/LoginUtil.java
@@ -14,6 +14,9 @@ public class LoginUtil {
 
     public static void markAsLogin(Channel channel) {
         channel.attr(Attributes.LOGIN).set(true);
+
+        Boolean b = channel.attr(Attributes.LOGIN).get();
+        System.out.println(b);
     }
 
     public static boolean hasLogin(Channel channel) {
